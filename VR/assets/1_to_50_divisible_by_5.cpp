573f15c08a3bb407a08bf140b78d9e613b5c09f0
Vedant Raut <85823297+VedantRaut1@users.noreply.github.com>
2021-11-07 14:01:45 +0530

    Add files via upload

---
+++ VR/assets/1_to_50_divisible_by_5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int n = 1000;
+    int i = 1;
+
+    for(i == 1;i<=n;i++){
+        if(i%5==0){
+            cout << i << endl;
+
+        }
+
+    }
+
+
+    return 0;
+}
+
